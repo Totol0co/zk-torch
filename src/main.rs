7fedfd18001fab1771c683e87010294b7328d4e4
Ari Fiorino <arifiorino@gmail.com>
2023-11-30 23:28:03 -0600

    efficiencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use ark_ec::Group;
 use ark_poly::evaluations::univariate::Evaluations;
 use ark_poly::{Polynomial, GeneralEvaluationDomain, EvaluationDomain};
-use ark_bls12_381::Fr;
+use ark_msm::msm::{VariableBaseMSM};
+use ark_bls12_381::{Fr, G1Projective, G1Affine, G2Projective, G2Affine};
 use ark_std::{ops::Mul, ops::Sub, UniformRand};
 use std::time::Instant;
 const N : usize = 1<<14;
 
 fn main() {
   let mut rng = ark_std::test_rng();
+
+  let x = Fr::rand(&mut rng);
+
+  let mut srs_x = [x; N];
+  let mut x2 = x;
+  for i in 0..N{
+    srs_x[i]=x2;
+    x2*=x2;
+  }
+  let bases = [G1Projective::generator().into() ; N];
+  let srs = VariableBaseMSM::multi_scalar_mul(&bases, &srs_x);
+
   let domain  = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
   let mut a = Vec::new();
   let mut b = Vec::new();
