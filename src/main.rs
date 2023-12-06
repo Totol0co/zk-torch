28f4d75a95e57d6af43952e4ec3f483596490184
Ari Fiorino <arifiorino@gmail.com>
2023-12-06 01:44:00 -0500

    pairing + 0.4 msm

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use ark_ec::Group;
+use ark_ec::AffineRepr;
+use ark_ec::VariableBaseMSM;
+use ark_ec::pairing::Pairing;
 use ark_poly::evaluations::univariate::Evaluations;
-use ark_poly::{Polynomial, GeneralEvaluationDomain, EvaluationDomain};
-use ark_msm::msm::{VariableBaseMSM};
-use ark_bls12_381::{Fr, G1Projective, G1Affine, G2Projective, G2Affine};
+use ark_poly::{GeneralEvaluationDomain, EvaluationDomain};
+use ark_bls12_381::{Fr, G1Projective, G1Affine, G2Projective, G2Affine, Bls12_381};
 use ark_std::{ops::Mul, ops::Sub, UniformRand};
 use std::time::Instant;
-const N : usize = 1<<14;
+const N : usize = 1<<10;
 
 fn main() {
   let mut rng = ark_std::test_rng();
 
   let x = Fr::rand(&mut rng);
 
-  let mut srs_x = [x; N];
-  let mut x2 = x;
-  for i in 0..N{
-    srs_x[i]=x2;
-    x2*=x2;
+  let mut xp = x;
+  let mut srs = [G1Affine::generator() ; N];
+  for i in 1..N{
+    srs[i]=(srs[i]*xp).into();
+    xp*=x;
   }
-  let bases = [G1Projective::generator().into() ; N];
-  let srs = VariableBaseMSM::multi_scalar_mul(&bases, &srs_x);
+  xp = x;
+  let mut srs2 = [G2Affine::generator() ; N+1];
+  for i in 1..N+1{
+    srs2[i]=(srs2[i]*xp).into();
+    xp*=x;
+  }
 
   let domain  = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
   let mut a = Vec::new();
@@ -35,13 +40,20 @@ fn main() {
   let f = Evaluations::from_vec_and_domain(a, domain).interpolate();
   let g = Evaluations::from_vec_and_domain(b, domain).interpolate();
   let h = Evaluations::from_vec_and_domain(c, domain).interpolate();
+  let fx = G1Projective::msm(&srs, &f.coeffs).unwrap();
+  let gx2 = G2Projective::msm(&srs2[..N], &g.coeffs).unwrap();
+  let hx = G1Projective::msm(&srs, &h.coeffs).unwrap();
+  let zx2 = srs2[N] - srs2[0];
 
   let start = Instant::now();
   let t = f.mul(&g).sub(&h).divide_by_vanishing_poly(domain).unwrap().0;
-  let gamma = Fr::rand(&mut rng);
-  let lhs = f.evaluate(&gamma) * g.evaluate(&gamma) - h.evaluate(&gamma);
-  let rhs = t.evaluate(&gamma) * domain.vanishing_polynomial().evaluate(&gamma);
+  println!("{:?}", start.elapsed());
+  let tx = G1Projective::msm(&srs[..N-1], &t.coeffs).unwrap();
+  println!("{:?}", start.elapsed());
+
+  let lhs = Bls12_381::pairing(fx,gx2) - Bls12_381::pairing(hx,srs2[0]);
+  let rhs = Bls12_381::pairing(tx,zx2);
+  
   assert!(lhs==rhs);
   println!("{:?}", start.elapsed());
 }
-
