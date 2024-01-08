8bce68a2d4047dfa32d1b09c0d43297b5a1d14f0
Ari Fiorino <arifiorino@gmail.com>
2024-01-08 10:40:30 -0500

    Add model to prove and verify

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,17 @@ trait BasicBlock{
          inputs: &[&[Fr]]) ->
          Vec<Fr>;
   fn setup(srs: (&[G1Affine],&[G2Affine]),
-           model: &[Fr]) ->
+           model: &mut Data) ->
           (Vec<G1Affine>,Vec<G2Affine>);
   fn prove<R: Rng>(srs: (&[G1Affine],&[G2Affine]),
                    setup: (&[G1Affine],&[G2Affine]),
+                   model: &Data,
                    inputs: &[&Data],
                    output: &Data,
                    rng: &mut R) ->
                   (Vec<G1Affine>,Vec<G2Affine>);
   fn verify<R: Rng>(srs: (&[G1Affine],&[G2Affine]),
+                    model: &DataEnc,
                     inputs: &[&DataEnc],
                     output: &DataEnc,
                     proof: (&[G1Affine],&[G2Affine]),
@@ -46,24 +48,26 @@ impl BasicBlock for MulBasicBlock{
     return r;
   }
   fn setup(srs: (&[G1Affine],&[G2Affine]),
-           model: &[Fr]) ->
+           model: &mut Data) ->
           (Vec<G1Affine>,Vec<G2Affine>){
     return (Vec::new(), Vec::new());
   }
   fn prove<R: Rng>(srs: (&[G1Affine],&[G2Affine]),
                    setup: (&[G1Affine],&[G2Affine]),
+                   model: &Data,
                    inputs: &[&Data],
                    output: &Data,
                    rng: &mut R) ->
                   (Vec<G1Affine>,Vec<G2Affine>){
     let N = inputs[0].raw.len();
-    let domain  = GeneralEvaluationDomain::<Fr>::new(N).unwrap(); //TODO: cache domain generation
+    let domain  = GeneralEvaluationDomain::<Fr>::new(N).unwrap();
     let gx2 = G2Projective::msm(&srs.1[..N], &inputs[1].poly.coeffs).unwrap().into();
     let t = inputs[0].poly.mul(&inputs[1].poly).sub(&output.poly).divide_by_vanishing_poly(domain).unwrap().0;
     let tx = G1Projective::msm(&srs.0[..N-1], &t.coeffs).unwrap().into();
     return (vec![tx],vec![gx2]);
   }
   fn verify<R: Rng>(srs: (&[G1Affine],&[G2Affine]),
+                    model: &DataEnc,
                     inputs: &[&DataEnc],
                     output: &DataEnc,
                     proof: (&[G1Affine],&[G2Affine]),
@@ -104,7 +108,8 @@ fn main() {
   }
   let inputs : Vec<&[Fr]> = vec![&a, &b];
   let c = MulBasicBlock::run(&Vec::new(),&inputs);
-  let setup = MulBasicBlock::setup(srs,&Vec::new());
+  let mut model = Data{raw: Vec::new(), poly: DensePolynomial{coeffs:Vec::new()}, g1: G1Affine::generator()};
+  let setup = MulBasicBlock::setup(srs,&mut model);
   let f = DensePolynomial{coeffs: domain.ifft(&a)};
   let g = DensePolynomial{coeffs: domain.ifft(&b)};
   let h = DensePolynomial{coeffs: domain.ifft(&c)};
@@ -115,10 +120,11 @@ fn main() {
   let b = Data{raw: b, poly: g, g1: gx};
   let c = Data{raw: c, poly: h, g1: hx};
   let inputs : Vec<&Data> = vec![&a, &b];
-  let proof = MulBasicBlock::prove(srs,(&(setup.0),&(setup.1)),&inputs,&c,&mut rng);
+  let proof = MulBasicBlock::prove(srs,(&(setup.0),&(setup.1)),&model,&inputs,&c,&mut rng);
   let a = DataEnc{len: a.raw.len(), g1: a.g1};
   let b = DataEnc{len: b.raw.len(), g1: b.g1};
   let c = DataEnc{len: c.raw.len(), g1: c.g1};
   let inputs : Vec<&DataEnc> = vec![&a, &b];
-  MulBasicBlock::verify(srs,&inputs,&c,(&(proof.0),&(proof.1)),&mut rng);
+  let model = DataEnc{len: 0, g1: G1Affine::generator()};
+  MulBasicBlock::verify(srs,&model,&inputs,&c,(&(proof.0),&(proof.1)),&mut rng);
 }
