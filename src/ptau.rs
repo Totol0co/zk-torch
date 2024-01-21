2269b2d2b3387ae6170f6a6efe601eb7937a3245
Ari Fiorino <arifiorino@gmail.com>
2024-01-21 15:16:48 -0600

    Faster/cleaner loading of ptau file

--- src/ptau.rs
+++ src/ptau.rs
@@ -1,25 +1,29 @@
-use powersoftau::parameters::{CeremonyParams, CheckForCorrectness, UseCompression};
-use powersoftau::batched_accumulator::BatchedAccumulator;
-use pairing_ce::bn256::Bn256;
-use ff_ce::PrimeField;
-use memmap::MmapOptions;
-use std::fs::OpenOptions;
 use ark_bn254::{Fq, Fq2, G1Affine, G2Affine};
-use ark_ff::biginteger::BigInt;
+use ark_ff::PrimeField;
+use std::fs;
+use rayon::prelude::*;
 
 pub fn load_file(filename: &str, n: usize) -> (Vec<G1Affine>,Vec<G2Affine>){
-  let parameters = CeremonyParams::<Bn256>::new(n, 2097152);
-  let reader = OpenOptions::new().read(true).open(filename).unwrap();
-  assert!(reader.metadata().unwrap().len() == (parameters.accumulator_size as u64));
-  let readable_map = unsafe{
-    MmapOptions::new().map(&reader).unwrap()
-  };
-  let acc = BatchedAccumulator::deserialize(&readable_map, CheckForCorrectness::No, UseCompression::No, &parameters).unwrap();
-  let temp  = acc.tau_powers_g1.iter().map(|x| G1Affine::new(Fq::new(BigInt::new(x.get_x().into_repr().0)),
-                                                             Fq::new(BigInt::new(x.get_y().into_repr().0)))).collect();
-  let temp2 = acc.tau_powers_g2.iter().map(|x| G2Affine::new(Fq2{c0: Fq::new(BigInt::new(x.get_x().c0.into_repr().0)),
-                                                                 c1: Fq::new(BigInt::new(x.get_x().c1.into_repr().0))},
-                                                             Fq2{c0: Fq::new(BigInt::new(x.get_y().c0.into_repr().0)),
-                                                                 c1: Fq::new(BigInt::new(x.get_y().c1.into_repr().0))})).collect();
-  return (temp, temp2);
+  let bytes = fs::read(filename).unwrap();
+
+  let powers_length=1<<n;
+  let powers_g1_length=(powers_length<<1)-1;
+
+  let g1 = (0..powers_g1_length).into_par_iter().map(|i|{
+    let start = 64 + i*64;
+    let x = Fq::from_be_bytes_mod_order(&bytes[start..start+32]);
+    let y = Fq::from_be_bytes_mod_order(&bytes[start+32..start+64]);
+    G1Affine::new_unchecked(x,y)
+  }).collect();
+
+  let g2 = (0..powers_length).into_par_iter().map(|i|{
+    let start = 64 + 64*powers_g1_length + 128*i;
+    let a = Fq::from_be_bytes_mod_order(&bytes[start..start+32]);
+    let b = Fq::from_be_bytes_mod_order(&bytes[start+32..start+64]);
+    let c = Fq::from_be_bytes_mod_order(&bytes[start+64..start+96]);
+    let d = Fq::from_be_bytes_mod_order(&bytes[start+96..start+128]);
+    G2Affine::new_unchecked(Fq2{c0:b, c1:a}, Fq2{c0:d, c1:c})
+  }).collect();
+
+  return (g1,g2);
 }
