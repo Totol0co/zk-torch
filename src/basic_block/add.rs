f60eb76bd1991187f544af024709bc8ebb29123a
Ari Fiorino <arifiorino@gmail.com>
2024-01-29 12:11:45 -0600

    Ran rustfmt

--- src/basic_block/add.rs
+++ src/basic_block/add.rs
@@ -1,28 +1,27 @@
+use super::{BasicBlock, DataEnc};
 use ark_bn254::{Fr, G1Affine, G2Affine};
 use rand::Rng;
-use super::{BasicBlock,DataEnc};
 
 pub struct AddBasicBlock;
-impl BasicBlock for AddBasicBlock{
-  fn run(_model: &Vec<Fr>,
-         inputs: &Vec<Vec<Fr>>) ->
-         Vec<Fr>{
+impl BasicBlock for AddBasicBlock {
+  fn run(_model: &Vec<Fr>, inputs: &Vec<Vec<Fr>>) -> Vec<Fr> {
     let mut r = Vec::new();
-    for i in 0..inputs[0].len(){
-      r.push(inputs[0][i]+inputs[1][i]);
+    for i in 0..inputs[0].len() {
+      r.push(inputs[0][i] + inputs[1][i]);
     }
     return r;
   }
-  fn verify<R: Rng>(_srs: (&Vec<G1Affine>,&Vec<G2Affine>),
-                    _model: &DataEnc,
-                    inputs: &Vec<DataEnc>,
-                    output: &DataEnc,
-                    _proof: (&Vec<G1Affine>,&Vec<G2Affine>),
-                    _rng: &mut R){
+  fn verify<R: Rng>(
+    _srs: (&Vec<G1Affine>, &Vec<G2Affine>),
+    _model: &DataEnc,
+    inputs: &Vec<DataEnc>,
+    output: &DataEnc,
+    _proof: (&Vec<G1Affine>, &Vec<G2Affine>),
+    _rng: &mut R,
+  ) {
     // Verify f(x)+g(x)=h(x)
-    let lhs = inputs[0].g1+inputs[1].g1;
+    let lhs = inputs[0].g1 + inputs[1].g1;
     let rhs = output.g1;
-    assert!(lhs==rhs);
+    assert!(lhs == rhs);
   }
 }
-
--- src/ptau.rs
+++ src/ptau.rs
@@ -1,29 +1,35 @@
 use ark_bn254::{Fq, Fq2, G1Affine, G2Affine};
 use ark_ff::PrimeField;
-use std::fs;
 use rayon::prelude::*;
+use std::fs;
 
-pub fn load_file(filename: &str, n: usize) -> (Vec<G1Affine>,Vec<G2Affine>){
+pub fn load_file(filename: &str, n: usize) -> (Vec<G1Affine>, Vec<G2Affine>) {
   let bytes = fs::read(filename).unwrap();
 
-  let powers_length=1<<n;
-  let powers_g1_length=(powers_length<<1)-1;
+  let powers_length = 1 << n;
+  let powers_g1_length = (powers_length << 1) - 1;
 
-  let g1 = (0..powers_g1_length).into_par_iter().map(|i|{
-    let start = 64 + i*64;
-    let x = Fq::from_be_bytes_mod_order(&bytes[start..start+32]);
-    let y = Fq::from_be_bytes_mod_order(&bytes[start+32..start+64]);
-    G1Affine::new_unchecked(x,y)
-  }).collect();
+  let g1 = (0..powers_g1_length)
+    .into_par_iter()
+    .map(|i| {
+      let start = 64 + i * 64;
+      let x = Fq::from_be_bytes_mod_order(&bytes[start..start + 32]);
+      let y = Fq::from_be_bytes_mod_order(&bytes[start + 32..start + 64]);
+      G1Affine::new_unchecked(x, y)
+    })
+    .collect();
 
-  let g2 = (0..powers_length).into_par_iter().map(|i|{
-    let start = 64 + 64*powers_g1_length + 128*i;
-    let a = Fq::from_be_bytes_mod_order(&bytes[start..start+32]);
-    let b = Fq::from_be_bytes_mod_order(&bytes[start+32..start+64]);
-    let c = Fq::from_be_bytes_mod_order(&bytes[start+64..start+96]);
-    let d = Fq::from_be_bytes_mod_order(&bytes[start+96..start+128]);
-    G2Affine::new_unchecked(Fq2{c0:b, c1:a}, Fq2{c0:d, c1:c})
-  }).collect();
+  let g2 = (0..powers_length)
+    .into_par_iter()
+    .map(|i| {
+      let start = 64 + 64 * powers_g1_length + 128 * i;
+      let a = Fq::from_be_bytes_mod_order(&bytes[start..start + 32]);
+      let b = Fq::from_be_bytes_mod_order(&bytes[start + 32..start + 64]);
+      let c = Fq::from_be_bytes_mod_order(&bytes[start + 64..start + 96]);
+      let d = Fq::from_be_bytes_mod_order(&bytes[start + 96..start + 128]);
+      G2Affine::new_unchecked(Fq2 { c0: b, c1: a }, Fq2 { c0: d, c1: c })
+    })
+    .collect();
 
-  return (g1,g2);
+  return (g1, g2);
 }
